31230c4491a46a25a07fc19b9d67639f871b9b03
Alexander Kochurov <akrendering@mail.ru>
2022-09-09 04:27:48 +0300

    added go-collector and results

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # go-vs-faust
 Performance under load for basic Go and Faust applications.
 
-First run on local machine got me something like this:  
+Comparing senders:
 py-sender (FastAPI, Faust, 100 locust users):  58% CPU usage, 0.4% memory usage   
-go-sender (FastAPI, goka,  100 locust users): 6.5% CPU usage, 0.1% memory usage
+go-sender (FastAPI, goka,  100 locust users): 6.5% CPU usage, 0.1% memory usage   
+
+Comparing collectors:   
+go-collector (basic net/http) uses 3-4 times less CPU resources than fastapi-collector under the same load (go-sender, 100 locust users)
+
--- go-collector/main.go
+++ go-collector/main.go
@@ -1,6 +1,85 @@
 package main
 
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/Shopify/sarama"
+	"github.com/lovoo/goka"
+	"github.com/lovoo/goka/codec"
+)
+
+type RawEvent struct {
+	Source string 		`json:"source"`
+	EventName string 	`json:"event_name"`
+	EventStatus string 	`json:"event_status"`
+	Created string 		`json:"created"`
+	Payout string 		`json:"payout"`
+}
+
+var (
+	// brokers = []string{"localhost:9093"}
+	brokers = []string{"kafka-go-vs-faust:9092"}
+	port = "0.0.0.0:5005"
+	topic goka.Stream = "test-events-from"
+
+	tmc *goka.TopicManagerConfig
+	cfg *sarama.Config
+	Emitter *goka.Emitter
+)
+
+func init() {
+	tmc = goka.NewTopicManagerConfig()
+	tmc.Table.Replication = 1
+	tmc.Stream.Replication = 1
+
+	cfg = goka.DefaultConfig()
+	cfg.Version = sarama.V2_4_0_0
+	// cfg.Net.SASL.Enable = true
+	// cfg.Net.SASL.User = "username"
+	// cfg.Net.SASL.Password = "password"
+	goka.ReplaceGlobalConfig(cfg)
+}
 
 func main() {
-	
+	var err error
+	Emitter, err = goka.NewEmitter(brokers, topic, new(codec.String))
+	if err != nil { log.Fatalf("error creating emitter: %v", err) }
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	server := &http.Server{
+		Addr: port,
+		Handler: mux,
+	}
+
+	log.Printf("Starting server on %s...", port)
+	err = server.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		log.Println("...server closed")
+	} else if err != nil {
+		log.Printf("Error starting server: %s", err)
+	}
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	event := RawEvent{
+		Source: r.URL.Query().Get("source"),
+		EventName: r.URL.Query().Get("event_name"),
+		EventStatus: r.URL.Query().Get("event_status"),
+		Created: r.URL.Query().Get("created"),
+		Payout: r.URL.Query().Get("payout"),
+	}
+	marshalled, err := json.Marshal(event)
+	serialized := string(marshalled)
+
+	if err == nil {
+		io.WriteString(w, "OK")
+		log.Printf("received OK: %s", serialized)
+		e := Emitter.EmitSync("key", serialized)
+		if e == nil { log.Println("sent OK") }
+	}	
 }
